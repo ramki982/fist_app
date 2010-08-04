c73712eb7c9f15e95c3c7aca39a20c597fb0861c
ramki982 <ramki982@gmail.com>
2010-08-04 09:31:47 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FistApp::Application.config.secret_token = '57ec1ef2bfa0689bebfc2897241ec9040928cd0285186179dee4962ebd1c52a650c3d192acb0948225ba2a72cbb55c96fe623153adbb1f9ea8801e24bd8c924b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FistApp::Application.config.session_store :cookie_store, :key => '_fist_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# FistApp::Application.config.session_store :active_record_store
